69e1fc0a8a403e3896845ed16b397d7f8c28a508
Anton S <residentsummer@gmail.com>
2017-01-21 20:43:03 +0300

    open android messages backup

--- WhatsAppImporter/main.m
+++ WhatsAppImporter/main.m
@@ -8,22 +8,27 @@
 
 #import <Foundation/Foundation.h>
 #import <CoreData/CoreData.h>
+#import <sqlite3.h>
 
 @interface Importer : NSObject
 - (void) initializeCoreDataWithMomd:(NSString *)momdPath andDatabase:(NSString *)dbPath;
+- (void) initializeAndroidStoreFromPath:(NSString *)storePath;
 - (void) dumpEntityDescriptions;
-- (void) peekMessages;
+- (void) peekAndroidMessages;
+- (void) peekiOSMessages;
 
 @property (nonatomic, strong) NSManagedObjectContext *moc;
 @property (nonatomic, strong) NSManagedObjectModel *mom;
 @property (nonatomic, strong) NSPersistentStore *store;
+@property (nonatomic) sqlite3 *androidStore;
 @end
 
 int main(int argc, const char * argv[]) {
-    if (argc < 3) {
-        NSLog(@"usage: %s <momd> <iphone sqlite>", argv[0]);
+    if (argc < 4) {
+        NSLog(@"usage: %s <android sqlite> <momd> <iphone sqlite>", argv[0]);
         return 1;
     }
+
     @autoreleasepool {
         NSMutableArray *args = [NSMutableArray new];
         // Skip executable name
@@ -33,10 +38,11 @@ int main(int argc, const char * argv[]) {
         }
 
         Importer *imp = [Importer new];
-        [imp initializeCoreDataWithMomd:[args objectAtIndex:0]
-                            andDatabase:[args objectAtIndex:1]];
+        [imp initializeAndroidStoreFromPath:[args objectAtIndex:0]];
+        [imp initializeCoreDataWithMomd:[args objectAtIndex:1]
+                            andDatabase:[args objectAtIndex:2]];
         //[imp dumpEntityDescriptions];
-        [imp peekMessages];
+        [imp peekAndroidMessages];
     }
     return 0;
 }
@@ -49,7 +55,7 @@ int main(int argc, const char * argv[]) {
     NSManagedObjectModel *mom = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
     NSAssert(mom != nil, @"Error initializing Managed Object Model");
     self.mom = mom;
-    
+
     NSPersistentStoreCoordinator *psc = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:mom];
     NSManagedObjectContext *moc = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSMainQueueConcurrencyType];
     [moc setPersistentStoreCoordinator:psc];
@@ -59,9 +65,20 @@ int main(int argc, const char * argv[]) {
     NSURL *storeURL = [NSURL fileURLWithPath:dbPath];
     NSPersistentStore *store = [psc addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error];
     NSAssert(store != nil, @"Error initializing PSC: %@\n%@", [error localizedDescription], [error userInfo]);
-    
+
     self.store = store;
     NSLog(@"CoreData loaded");
+}
+
+- (void) initializeAndroidStoreFromPath:(NSString *)storePath {
+    sqlite3 *store = nil;
+
+    if (sqlite3_open([storePath UTF8String], &store) != SQLITE_OK) {
+        NSLog(@"%s", sqlite3_errmsg(store));
+    }
+
+    NSLog(@"Android store loaded");
+    self.androidStore = store;
 }
 
 - (void) dumpEntityDescriptions {
@@ -71,7 +88,7 @@ int main(int argc, const char * argv[]) {
     }
 }
 
-- (void) peekMessages {
+- (void) peekiOSMessages {
     NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:@"WAMessage"];
     fetchRequest.returnsObjectsAsFaults = NO;
     [fetchRequest setFetchLimit:100];
@@ -85,6 +102,59 @@ int main(int argc, const char * argv[]) {
 
     for (NSManagedObject *msg in results) {
         NSLog(@"%@", [msg valueForKey:@"text"]);
+    }
+}
+
+- (NSMutableArray *) executeQuery:(NSString *)query {
+    NSMutableArray *results = [NSMutableArray new];
+    NSMutableArray *columnNames = nil;
+    sqlite3_stmt *prepared;
+    int totalColumns = 0;
+    int result = FALSE;
+
+    result = sqlite3_prepare_v2(self.androidStore, [query UTF8String], -1, &prepared, NULL);
+    totalColumns = sqlite3_column_count(prepared);
+    if (result != SQLITE_OK) {
+        NSLog(@"%s", sqlite3_errmsg(self.androidStore));
+        abort();
+    }
+
+    // It's a SELECT
+    if (totalColumns > 0) {
+        columnNames = [NSMutableArray arrayWithCapacity:totalColumns];
+        for (int i = 0; i < totalColumns; ++i) {
+            [columnNames addObject:[NSString stringWithUTF8String:sqlite3_column_name(prepared, i)]];
+        }
+    }
+
+    // Fetching rows one by one
+    while ((result = sqlite3_step(prepared)) == SQLITE_ROW) {
+        NSMutableArray *row = [NSMutableArray arrayWithCapacity:totalColumns];
+        int columnsCount = sqlite3_data_count(prepared);
+
+        for (int i = 0; i < columnsCount; ++i) {
+            const char *text = (const char *) sqlite3_column_text(prepared, i);
+            if (text) {
+                [row addObject:[NSString stringWithUTF8String:text]];
+            } else {
+                [row addObject:[NSNull null]];
+            }
+        }
+
+        [results addObject:[NSDictionary dictionaryWithObjects:row
+                                                       forKeys:columnNames]];
+    }
+
+    sqlite3_finalize(prepared);
+    return results;
+}
+
+- (void) peekAndroidMessages {
+    NSMutableArray *results = nil;
+    results = [self executeQuery:@"SELECT * FROM messages LIMIT 100;"];
+
+    for (NSDictionary *row in results) {
+        NSLog(@"%@", [row objectForKey:@"data"]);
     }
 }
 
